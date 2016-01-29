09daf83ca889aca24dc861ea562b9078013883ab
fernandosoto <fernandosoto_@hotmail.com>
2016-01-29 10:47:51 +0100

    Spring MVC
    Hello World Spring MVC

---
+++ MappleTechProj/src/main/java/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") 
+			String name)
+	{
+		System.out.println("Controller");
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message","Hello ");
+		mv.addObject("name", name);
+		return mv;
+	}
+
+}
