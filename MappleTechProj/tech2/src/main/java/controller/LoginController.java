41979acf911de8e083dc7e3a07c5e7a5f63efd6a
Thomas <brandertb@gmail.com>
2016-02-01 15:29:05 +0100

    password hash

--- MappleTechProj/tech2/src/main/java/controller/LoginController.java
+++ MappleTechProj/tech2/src/main/java/controller/LoginController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -8,30 +12,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import forms.UserForm;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import vm.UserVM;
 
 @Controller
 public class LoginController {
 
-	private String name, password;
-	private UserForm userForm = new UserForm();
+	private UserVM userVm = new UserVM();
+	private ObjectMapper mapper = new ObjectMapper();
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView login(ModelMap model) {
-		System.out.println("HEEEEEEJ");
 		
-		model.addAttribute("user", userForm);
-        //return "login/login";
+		model.addAttribute("user", this.userVm);
 		ModelAndView mv = new ModelAndView("login/login");
-		mv.addObject("user", userForm);
+		mv.addObject("user", this.userVm);
 		return mv;
 	}
 
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(@ModelAttribute("user") UserForm userForm, Model model) {
-		System.out.println("Username: " + userForm.getUsername());
+	public String login(@ModelAttribute("user") UserVM userVm, Model model) {
+		
+		System.out.println("Username: "+userVm.getUsername()+"pwd: "+userVm.getPassword());
+		try {
+			UserVM loggedInUser = new UserVM(userVm.getUsername(), passwordHash(userVm.getPassword()));
+			String jsonString = mapper.writeValueAsString(loggedInUser);
+		} catch (JsonProcessingException | NoSuchAlgorithmException e) {
+			System.out.println("Error: "+e.getStackTrace());
+		}finally{
+			//REST --> Skicka jsonString
+			
+		}
 		return "redirect:/";
+	}
+	
+	
+	private String passwordHash(String pwd) throws NoSuchAlgorithmException{
+		
+		MessageDigest msgDigest = MessageDigest.getInstance("SHA-256");
+		msgDigest.update(userVm.getPassword().getBytes());
+		byte[] digest = msgDigest.digest();
+		return digest.toString();
+		
 	}
 
 }
