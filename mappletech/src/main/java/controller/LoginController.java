aefdc02b046353a65c8e631198d90a64b050caf6
Rami <rami.alsawadi1@gmail.com>
2016-02-09 13:24:41 +0100

    redirect test

--- mappletech/src/main/java/controller/LoginController.java
+++ mappletech/src/main/java/controller/LoginController.java
@@ -16,38 +16,40 @@ import vm.UserVM;
 @Controller
 public class LoginController {
 	private final String URI = "http://130.237.84.211:8080/mappletech/rest/login";
+	private ModelAndView loginMV = new ModelAndView("login/index");
 
 	// Omdirigerar just nu till login
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
 	public String start() {
-		
 		return "redirect:/login";
 	}
-	
+
 	@RequestMapping(value = { "/login" }, method = RequestMethod.GET)
 	public ModelAndView login() {
 		System.out.println("In GET Login...");
-		ModelAndView mv = new ModelAndView("login/index");
-		mv.addObject("uservm", new UserVM());
-		return mv;
+		loginMV.addObject("uservm", new UserVM());
+		return loginMV;
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String doLogin(@ModelAttribute("uservm") UserVM userVm, Model model) {
+	public ModelAndView doLogin(@ModelAttribute("uservm") UserVM userVm, Model model) {
 
-			System.out.println("In POST Login...");
-			UserVM loggedInUser = new UserVM(userVm.getUsername(), passwordHash(userVm.getPassword()));
-			RestTemplate restTemplate = new RestTemplate();
-			UserVM u = restTemplate.postForObject(URI, loggedInUser, UserVM.class);
-			if (u != null) {
-				System.out.println("SUCCESS");
-				System.out.println(u.getUsername() + " " + u.getFullName() + " ");
-			}
-
-		return "redirect:/";
+		System.out.println("In POST Login...");
+		UserVM loggedInUser = new UserVM(userVm.getUsername(), passwordHash(userVm.getPassword()));
+		RestTemplate restTemplate = new RestTemplate();
+		UserVM u = restTemplate.postForObject(URI, loggedInUser, UserVM.class);
+		if (u != null) {
+			System.out.println("SUCCESS");
+			ModelAndView mv = new ModelAndView("/index");
+			mv.addObject("sessUser", u);
+			return mv;
+		} else {
+			System.out.println("FAILED");
+			return loginMV;
+		}
 	}
 
-	private String passwordHash(String pwd){
+	private String passwordHash(String pwd) {
 
 		MessageDigest msgDigest = null;
 		try {
@@ -58,9 +60,9 @@ public class LoginController {
 		msgDigest.update(pwd.getBytes());
 		byte[] digest = msgDigest.digest();
 		StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < digest.length; i++) {
-         sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
-        }
+		for (int i = 0; i < digest.length; i++) {
+			sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+		}
 		return sb.toString();
 	}
 
