79897a2c4c4974deec1b0b829f5b1eca3328e2c0
Thomas <brandertb@gmail.com>
2016-02-10 12:24:31 +0100

    ändring adminController

--- mappletech/src/main/java/controller/AdminController.java
+++ mappletech/src/main/java/controller/AdminController.java
@@ -16,18 +16,17 @@ public class AdminController {
 
 	private final String URI = "http://130.237.84.211:8080/mappletech/rest/admin";
 	
-	
-	@RequestMapping(value = { "/administrator/anvandare/" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/administrator/lagg-till-anvandare" }, method = RequestMethod.GET)
 	public ModelAndView addUsersGet() {
 		System.out.println("In GET anvandare hantera");
-		ModelAndView mv = new ModelAndView("administrator/anvandare/index");
+		ModelAndView mv = new ModelAndView("administrator/lagg-till-anvandare/index");
 		mv.addObject("uservm", new UserVM());
 		return mv;
 	}
 
-	@RequestMapping(value = "/administrator/anvandare/", method = RequestMethod.POST)
+	@RequestMapping(value = "/administrator/lagg-till-anvandare", method = RequestMethod.POST)
 	public String addUsersPost(@ModelAttribute("uservm") UserVM userVm, Model model) {
-
+		
 		System.out.println("In POST anvandare hantera");
 		UserVM newUser = new UserVM(userVm.getUsername(), generatePassword(), userVm.getFullName(), userVm.getPrivilege());
 		RestTemplate restTemplate = new RestTemplate();
@@ -35,22 +34,22 @@ public class AdminController {
 		
 		if (userExists) {
 			System.out.println("Failed");
-			return "redirect:/administrator/";
+			return "redirect:/administrator/lagg-till-anvandare";
 		}
 		System.out.println("Success!");
-		return "redirect:/administrator";
+		return "redirect:/administrator/anvandare";
 	}
 	
 	
-	@RequestMapping(value = { "/administrator/anvandare/editUser" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/administrator/anvandare" }, method = RequestMethod.GET)
 	public ModelAndView editUsersGet() {
 		System.out.println("In GET anvandare Edit...");
-		ModelAndView mv = new ModelAndView("administrator/anvandare");
+		ModelAndView mv = new ModelAndView("administrator/anvandare/index");
 		mv.addObject("uservm", new UserVM());
 		return mv;
 	}
 
-	@RequestMapping(value = "/administrator/anvandare/editUser", method = RequestMethod.POST)
+	@RequestMapping(value = "/administrator/anvandare", method = RequestMethod.POST)
 	public String editUsersPost(@ModelAttribute("uservm") UserVM userVm, Model model) {
 
 		System.out.println("In POST anvandare/editUser...");
