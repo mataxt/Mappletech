f76ce6550dd7461255890dadc148d00e7e08721a
fernandosoto <fernandosoto_@hotmail.com>
2016-02-11 12:54:52 +0100

    ProfileController
    Utan användare

--- mappletech/src/main/java/controller/ProfileController.java
+++ mappletech/src/main/java/controller/ProfileController.java
@@ -1,9 +1,11 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@RequestMapping("/Profil")
+@Controller
+@RequestMapping("/profil")
 public class ProfileController {
 	private final String URI = "http://130.237.84.211:8080/mappletech/rest/profile/";
 	
