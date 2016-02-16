c8453a787ebd667e0190aa937bc13e68755ab665
Rami <rami.alsawadi1@gmail.com>
2016-02-16 10:36:40 +0100

    relink to login if not logged in

--- mappletech/src/main/java/controller/FrontController.java
+++ mappletech/src/main/java/controller/FrontController.java
@@ -1,19 +1,25 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import vm.UserVM;
 
 @Controller
 @SessionAttributes("sessUser")
 public class FrontController {
 
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
-	public ModelAndView login() {
+	public ModelAndView login(Model model) {
 		System.out.println("In GET Main...");
-		return new ModelAndView("index");
+		if (model.containsAttribute("sessUser"))
+			return new ModelAndView("index");
+		else
+			return new ModelAndView("login/index","uservm",new UserVM());
 	}
 }
