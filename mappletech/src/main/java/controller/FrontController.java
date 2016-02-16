600766d80bee2a22eaca3833df23d43bda474977
Rami <rami.alsawadi1@gmail.com>
2016-02-16 10:40:56 +0100

    4real

--- mappletech/src/main/java/controller/FrontController.java
+++ mappletech/src/main/java/controller/FrontController.java
@@ -2,24 +2,21 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
-import vm.UserVM;
 
 @Controller
 @SessionAttributes("sessUser")
 public class FrontController {
 
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
-	public ModelAndView login(Model model) {
+	public String main(Model model) {
 		System.out.println("In GET Main...");
 		if (model.containsAttribute("sessUser"))
-			return new ModelAndView("index");
+			return "index";
 		else
-			return new ModelAndView("login/index","uservm",new UserVM());
+			return "redirect:login/index";
 	}
 }
