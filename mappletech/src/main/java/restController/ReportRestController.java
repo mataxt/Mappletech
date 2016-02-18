c3b3e95b741e66cc2496a6e79f0a0030309b57a9
fernandosoto <fernandosoto_@hotmail.com>
2016-02-18 14:36:58 +0100

    ReportRest
    rest add report

--- mappletech/src/main/java/restController/ReportRestController.java
+++ mappletech/src/main/java/restController/ReportRestController.java
@@ -6,12 +6,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import dao.ReportDAO;
+import dao.UserDAO;
 import model.Report;
 import vm.ReportVM;
 
 @RestController
 public class ReportRestController {
 
+	@RequestMapping(value = "/report/add")
+	public Boolean addReport(@RequestBody(required = true) ReportVM reportVM){
+		Report report = new Report();
+		report.setReportId(reportVM.getReportId());
+		report.setDescription(reportVM.getDescription());
+		report.setReason(reportVM.getReason());
+		report.setReporter(UserDAO.fetchUser(reportVM.getReporter()));
+		report.setDate(reportVM.getDate());
+		report.setStatus(reportVM.getStatus());
+		int i = ReportDAO.addReport(report);
+		if(i != 0)
+			return true;
+		else
+			return false;
+	}
+	
 	@RequestMapping(value = "/report/remove")
 	public Boolean removeReport(@RequestBody(required = true) ReportVM reportVM) {
 		Report report = new Report();
