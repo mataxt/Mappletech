bad078618610e15a4a4326498c2d70a0c311c7fd
fernandosoto <fernandosoto_@hotmail.com>
2016-02-22 14:18:45 +0100

    EventRest TEst

---
+++ mappletech/src/test/java/testrest/TestEventRest.java
@@ -0,0 +1,90 @@
+package testrest;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
+
+import dao.EventDAO;
+import model.Event;
+import model.User;
+import restController.EventRestController;
+import vm.EventVM;
+
+import static org.hamcrest.Matcher.*;
+import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(EventDAO.class)
+public class TestEventRest{
+	
+	
+	private Event first;
+	private User firstUser;
+	private Event second;
+	private User secondUser;
+	private Date currentDate;
+	
+	private MockMvc mockMvc;
+	private EventRestController eventRest;
+	
+	@Before
+	public void setUp()
+	{
+		firstUser = new User();
+		firstUser.setUsername("Fernando");
+		
+		secondUser = new User();
+		secondUser.setUsername("Rami");
+		
+		currentDate = new Date(System.currentTimeMillis());
+		first = new Event();
+		first.setCreator(firstUser);
+		first.setDate(currentDate);
+		first.setDescription("testDesc");
+		first.setEventID(1);
+		first.setTitle("testTitle");
+		
+		second = new Event();
+		second.setCreator(secondUser);
+		second.setDate(currentDate);
+		second.setDescription("secondTestDesc");
+		second.setEventID(2);
+		second.setTitle("secondTestTitle");
+		eventRest = new EventRestController();
+		mockMvc = MockMvcBuilders.standaloneSetup(eventRest).build();
+		
+	}
+	
+	@Test
+	public void getAllEventsTest() throws Exception
+	{
+		PowerMockito.mockStatic(EventDAO.class);
+		//when(EventDAO.getAllEvents()).thenReturn(Arrays.asList(first,second));
+		
+		mockMvc.perform(get("/event/getAll"))
+		.andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+	}
+}
