1fc98636ba6b00ba3797e84d3a6f524086ec0a93
Thomas <brandertb@gmail.com>
2016-03-04 11:19:12 +0100

    nu borde de funka..

--- mappletech/src/main/java/controller/AdminController.java
+++ mappletech/src/main/java/controller/AdminController.java
@@ -5,9 +5,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.servlet.http.HttpServletRequest;
-
-import org.apache.catalina.connector.Request;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
